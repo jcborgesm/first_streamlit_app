c6f67343ccba7d7215770f887f915cff82b48b13
jcborgesm <66495335+jcborgesm@users.noreply.github.com>
2023-08-31 15:16:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
 streamlit.title('First App Title')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omaga 3')
-streamlit.text('Kale')
-streamlit.text('Hard boiled egg')
-streamlit.text('')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣   Omaga 3')
+streamlit.text('🥗 Kale')
+streamlit.text('🐔 Hard boiled egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
